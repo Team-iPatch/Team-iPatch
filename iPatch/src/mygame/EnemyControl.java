2ed6953ae768b59e1fe3de6b173619454985e101
FMakosza <43878726+FMakosza@users.noreply.github.com>
2018-12-07 11:21:52 +0000

    Made cannon balls destroy on impact
    Added destroy() method to BulletControl
    Let EnemyControl destroy cannon balls that collide with it

--- iPatch/src/mygame/EnemyControl.java
+++ iPatch/src/mygame/EnemyControl.java
@@ -89,12 +89,14 @@ public class EnemyControl extends AbstractControl implements PhysicsCollisionLis
 
     @Override
     public void collision(PhysicsCollisionEvent event) {
-	if(!event.getNodeA().getName().equals("Scene") && !event.getNodeB().getName().equals("Scene")){
-	    System.out.print("nodeA: " + event.getNodeA().getName().toString() + "    nodeB: " + event.getNodeB().getName().toString() + "\n");
-	}
 	if(event.getNodeA().getName().equals("baddie") || event.getNodeB().getName().equals("baddie")){
 	    if(event.getNodeA().getName().equals("cannon ball") || event.getNodeB().getName().equals("cannon ball")){
 		this.hp -= 5;
+		
+		if(event.getNodeA().getName().equals("cannon ball"))
+		    event.getNodeA().getControl(BulletControl.class).destroy();
+		if(event.getNodeB().getName().equals("cannon ball"))
+		    event.getNodeB().getControl(BulletControl.class).destroy();
 	    }
 	}
 	
