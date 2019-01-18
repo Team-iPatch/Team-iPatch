93419b2cc4bfbcf2f2256de493e64b353b5e8f3e
FMakosza <43878726+FMakosza@users.noreply.github.com>
2019-01-18 22:19:12 +0000

    Implemented test UI
    Added simple button overlay, extensible with NiftyController.java

--- iPatch/src/mygame/Main.java
+++ iPatch/src/mygame/Main.java
@@ -27,6 +27,8 @@ import com.jme3.scene.shape.Box;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import de.lessvoid.nifty.Nifty;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -37,6 +39,7 @@ public class Main extends SimpleApplication {
 
     private BulletAppState bulletAppState;
     private PlayerControlState playerControlState;
+    private NiftyController niftyController;
     BetterCharacterControl character;
     Node ship;
     Node sceneNode;
@@ -59,13 +62,25 @@ public class Main extends SimpleApplication {
         loadPlayer();
         loadBox();
         loadBaddie();
-        loadMap();
+        loadGUI();
+        //loadMap();
         //bulletAppState.setDebugEnabled(true);
         BuildingGeneratorState b = new BuildingGeneratorState();
         b.generateDepartment("compsci", 5f, this);
         b.generateCollege("Derwent", new Vector3f(10,1,10), 5f, this);
     }
 	
+    private void loadGUI(){
+        niftyController = new NiftyController();
+        stateManager.attach(niftyController);
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        guiViewPort.addProcessor(niftyDisplay);
+        nifty.fromXml("Interface/Controls/mainMenu.xml", "start", niftyController);
+    
+        //nifty.setDebugOptionPanelColors(true);
+    }
+    
     private void loadEnemyGenerator(){
         enemyGenerator = new EnemyGenerator(assetManager, 
                                             bulletAppState.getPhysicsSpace());
@@ -138,4 +153,5 @@ public class Main extends SimpleApplication {
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
+
 }
